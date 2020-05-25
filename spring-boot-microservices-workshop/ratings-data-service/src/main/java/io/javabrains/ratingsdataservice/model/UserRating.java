132018749007ebac13638faf1676a844af543bf6
Ravi Sharma <ravisharma.1062@gmail.com>
2020-05-25 14:22:40 +0530

    Corrected Mongo Document

--- spring-boot-microservices-workshop/ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/model/UserRating.java
+++ spring-boot-microservices-workshop/ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/model/UserRating.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(value="User")
 public class UserRating {
 
 	@Id
