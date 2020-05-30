3e878c7940e0693131ad57d118f287c25a72caf5
Ravi Sharma <ravisharma.1062@gmail.com>
2020-05-30 11:39:53 +0530

    Implemented Lombok in all the modules

--- spring-boot-microservices-workshop/movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/models/Employee.java
+++ spring-boot-microservices-workshop/movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/models/Employee.java
@@ -3,23 +3,20 @@ package io.javabrains.moviecatalogservice.models;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Configuration
 @ConfigurationProperties("emp")
+@Setter
+@Getter
+@NoArgsConstructor
 public class Employee {
+	
 	private String name;
 	private String id;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
+	
 	@Override
 	public String toString() {
 		return "Employee [name=" + name + ", id=" + id + "]";
--- spring-boot-microservices-workshop/movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/models/UserRating.java
+++ spring-boot-microservices-workshop/movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/models/UserRating.java
@@ -2,25 +2,18 @@ package io.javabrains.moviecatalogservice.models;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRating {
 
     private String userId;
     private List<Rating> ratings;
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public List<Rating> getRatings() {
-        return ratings;
-    }
-
-    public void setRatings(List<Rating> ratings) {
-        this.ratings = ratings;
-    }
 
 }
