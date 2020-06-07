35146ba2bfaeb86a39ee532bfd6e250bf91e7a95
Ravi Sharma <ravisharma.1062@gmail.com>
2020-06-07 16:03:56 +0530

    minor changes

--- spring-boot-microservices-workshop/email-system/src/main/java/com/application/email/EmailSystemApplication.java
+++ spring-boot-microservices-workshop/email-system/src/main/java/com/application/email/EmailSystemApplication.java
@@ -3,8 +3,10 @@ package com.application.email;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@RefreshScope
 @SpringBootApplication
 @EnableEurekaClient
 public class EmailSystemApplication {
