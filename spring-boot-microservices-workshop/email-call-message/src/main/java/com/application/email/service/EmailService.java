a5450cc8450d78d99d953e8865c1d6f26630a9db
Ravi Sharma <ravisharma.1062@gmail.com>
2020-06-13 16:59:05 +0530

    Email List

--- spring-boot-microservices-workshop/email-call-message/src/main/java/com/application/email/service/EmailService.java
+++ spring-boot-microservices-workshop/email-call-message/src/main/java/com/application/email/service/EmailService.java
@@ -18,27 +18,30 @@ public class EmailService {
 
 	@Autowired
 	private JavaMailSender javaMailSender;
-	
+
 	@Autowired
 	private MailContentBuilder mailContentBuilder;
 
 	public String sendEmail(EmailDetails emailDetails) throws MessagingException {
-
+		String[] emailList = new String[emailDetails.getToAddress().size()];
+		int count = 0;
 		for(String toAddress : emailDetails.getToAddress()) {
-			MimeMessage message = javaMailSender.createMimeMessage();
-			MimeMessageHelper helper = new MimeMessageHelper(message, true);
-
-			helper.setTo(toAddress);
-			helper.setSubject(emailDetails.getSubject());
-			helper.setText(mailContentBuilder.build(emailDetails.getBody()), true);
-
-			if(emailDetails.getAttachmentPath() != null) {
-				FileSystemResource file = new FileSystemResource(new File(emailDetails.getAttachmentPath()));
-				helper.addAttachment("Test", file);
-			}
-
-			javaMailSender.send(message);
+			emailList[count++] = toAddress;
 		}
+		MimeMessage message = javaMailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+		helper.setTo(emailList);
+		helper.setSubject(emailDetails.getSubject());
+		helper.setText(mailContentBuilder.build(emailDetails.getBody()), true);
+
+		if(emailDetails.getAttachmentPath() != null) {
+			FileSystemResource file = new FileSystemResource(new File(emailDetails.getAttachmentPath()));
+			helper.addAttachment("Test", file);
+		}
+
+		javaMailSender.send(message);
+
 
 		return "Email Sent";
 	}
