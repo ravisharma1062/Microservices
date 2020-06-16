858b7d3d6fcd2f5a2c0c5675a177de6f9839b246
Ravi Sharma <ravisharma.1062@gmail.com>
2020-06-16 23:18:26 +0530

    Changes in kafka

--- spring-boot-microservices-workshop/spring-kafka-demo/src/main/java/com/example/demo/Producer.java
+++ spring-boot-microservices-workshop/spring-kafka-demo/src/main/java/com/example/demo/Producer.java
@@ -1,17 +1,37 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.KafkaException;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Service
 public class Producer {
-	
+
 	@Autowired
 	private KafkaTemplate<String, String> kafkaTemplate;
-	
+
 	public void sendMessage(String message) {
-		kafkaTemplate.send("email", message);
+		try {
+			ListenableFuture<SendResult<String, String>> result = kafkaTemplate.send("email", message);
+			result.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+
+				@Override
+				public void onSuccess(SendResult<String, String> result) {
+					// TODO Auto-generated method stub
+				}
+
+				@Override
+				public void onFailure(Throwable ex) {
+					System.out.println("In Producer: " + ex);
+					ListenableFuture<SendResult<String, String>> result = kafkaTemplate.send("email", message);
+				}
+			});
+		} catch (KafkaException e) {
+			System.out.println("In Producer: " + e);
+		}
 	}
-	
 }
