175b3cc01241f1546ebc440af15f532048f5dc9d
Ravi Sharma <ravisharma.1062@gmail.com>
2020-06-20 18:12:03 +0530

    OAuth Demo

---
+++ spring-boot-microservices-workshop/spring-oauth-demo/src/main/java/com/application/oauth/SpringOauthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.application.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauthDemoApplication.class, args);
+	}
+
+}
---
+++ spring-boot-microservices-workshop/spring-oauth-demo/src/test/java/com/application/oauth/SpringOauthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauthDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
