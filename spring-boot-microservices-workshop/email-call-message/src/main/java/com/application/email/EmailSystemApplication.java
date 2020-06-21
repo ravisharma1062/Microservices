890465712fbfe84b1e7bc96d8d0cabab552b2edc
Ravi Sharma <ravisharma.1062@gmail.com>
2020-06-21 14:23:11 +0530

    Added Scheduler

--- spring-boot-microservices-workshop/email-call-message/src/main/java/com/application/email/EmailSystemApplication.java
+++ spring-boot-microservices-workshop/email-call-message/src/main/java/com/application/email/EmailSystemApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @RefreshScope
 @SpringBootApplication
 @EnableEurekaClient
+@EnableScheduling
 public class EmailSystemApplication {
 
 	public static void main(String[] args) {
